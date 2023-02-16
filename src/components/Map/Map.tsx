a2f87a382ebb4e5f737b686fb081292a25048ccf
Leaundrae Mckinney <pytechdrae@gmail.com>
2023-02-16 08:10:13 -0500

    Interactive Map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -3,10 +3,10 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const Map = () => {
   return (
-    <MapContainer className={styles.mapContainer} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer className={styles.mapContainer} center={[51.505, -0.09]} zoom={18} scrollWheelZoom={true}>
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        url='https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'
       />
       <Marker position={[51.505, -0.09]}>
         <Popup>
